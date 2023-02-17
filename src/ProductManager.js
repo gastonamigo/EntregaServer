0f38c3ea75f542b3d5051d045c2fdbbc85eae12e
Gaston Amigo <gastonamigo@gmail.com>
2023-02-17 19:41:01 -0300

    modificacion del stock y app

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,12 +1,11 @@
 import fs from "fs";
 
 class ProductManager {
-    // #accId = 0;
     #path = "";
 
     constructor(path) {
         this.#path = path;
-    }
+    };
 
     //OBTENGO LA LISTA DE PRODUCTOS
     async getProducts() {
@@ -28,16 +27,15 @@ class ProductManager {
             return prod;
         } else {
             throw new Error(`Product ID: ${prodId} Not Found`);
-        }
-    }
+        };
+    };
     // AGREGA EL PRODUCTO 
     async addProduct(title, description, price, thumbnail, code, stock) {
-        
+
         const product = await this.getProducts();
-        
+
         const newProduct = {
-            // id: this.#acc, 
-            id: product.length+1,
+            id: product.length + 1,
             title,
             description,
             price,
@@ -48,7 +46,7 @@ class ProductManager {
         //CHECKEANDO SI FALTA INFORMACION
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             throw new Error("missing information");
-        }
+        };
 
         // CHECKEANDO QUE NO SE REPITA EL CODE       
         const checkCode = product.some((p) => p.code === code);
@@ -56,11 +54,9 @@ class ProductManager {
         if (checkCode) {
             throw new Error("product code already exist");
         } else {
-           await fs.promises.writeFile(this.#path, JSON.stringify([...product, newProduct]));
-            // this.#accId++;
-        }
-
-    }
+            await fs.promises.writeFile(this.#path, JSON.stringify([...product, newProduct]));
+        };
+    };
     //ACTUALIZA EL PRODUCTO
     async updateProduct(id, update) {
         const product = await this.getProducts();
@@ -68,14 +64,14 @@ class ProductManager {
 
         if (!productUpdated) {
             throw new Error("Product ID not found");//CHECKEA QUE EXISTA EL ID
-        }
+        };
 
         if (Object.keys(update).includes('code')) {
-            let checkCode = product.some(i => i.code === update.code)
+            let checkCode = product.some(i => i.code === update.code);
             if (checkCode) {
-                throw new Error("Product code modification not allowed")//CHECKEA SI SE ENVIA UN CODIGO PARA MODIFICAR
-            }
-        }
+                throw new Error("Product code modification not allowed");//CHECKEA SI SE ENVIA UN CODIGO PARA MODIFICAR
+            };
+        };
 
         productUpdated = { ...productUpdated, ...update };
         let newArray = product.filter(prod => prod.id !== id);
@@ -85,7 +81,7 @@ class ProductManager {
         await fs.promises.writeFile(this.#path, JSON.stringify(newArray));
 
         console.log('Updated Product');
-    }
+    };
 
     //BORRA EL PRODUCTO POR ID   
     async deleteProduct(prodId) {
@@ -96,30 +92,26 @@ class ProductManager {
 
         } else {
             let eraser = product.filter((p) => p.id !== prodId);
-           await fs.promises.writeFile(this.#path, JSON.stringify(eraser));
+            await fs.promises.writeFile(this.#path, JSON.stringify(eraser));
+        };
+    };
+};
 
-        }
-    }
-
-}
-
-
-
-
-async function main() {
-
-
-
-    const manager = new ProductManager('./products.json');
-
-    // console.log(await manager.getProducts()); // []
-    //   await manager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc123456", 25);
-    //   await manager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc12345", 25);
-    //   await manager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc1234", 25);
-    //   await manager.deleteProduct(1);
-    //   console.log(await manager.getProducts()); // []
-    //   console.log(await manager.getProductById(0));
-    //   console.log(await manager.getProductById(5));
-}
-// main();
 export default ProductManager;
+
+
+
+// async function main() {
+    //   const manager = new ProductManager('../products.json');
+//       console.log(await manager.getProducts()); // []
+// await manager.addProduct("Ender 3 v2", "Impresora 3d v2", 155000, "../img/ender-3-V2-01.jpg", "E01", 15);
+// await manager.addProduct("Ender 2 pro", "Impresora 3d pro", 145000, "../img/ender2pro1.png", "E02", 15);
+// await manager.addProduct("Cr10 pro v2", "Impresora 3d cr10 V2", 195000, "../img/creality-cr-10-smart-pro-1.jpg", "E03", 15);
+// await manager.addProduct("Cr5 pro", "Impresora 3d cr5 pro", 225000, "../img/cr5pro1.jpg", "E04", 15);
+// await manager.addProduct("Photon mono x", "Impresora de resina Mono X 4K", 225000, "../img/MonoX.jpg", "E05", 15);
+//       await manager.deleteProduct(1);
+//       console.log(await manager.getProducts()); // []
+//       console.log(await manager.getProductById(0));
+//       console.log(await manager.getProductById(5));
+// };
+// main();
--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
-import  express  from "express";
+import express from "express";
 import ProductManager from "./ProductManager.js";
 console.log(ProductManager);
 
-const manager = new ProductManager();
+const manager = new ProductManager("../products.json");
 
 
 const app = express();
+
+app.get("/products", async (req, res) => {
+
+    const { limit } = req.query;
+
+    if (!limit) {
+        const products = await manager.getProducts();
+        res.send(products);
+    } else {
+        const products = await manager.getProducts();
+        const limited = products.splice(0, limit);
+        res.send(limited);
+    };
+});
+
+app.get("/products/:id", async (req, res) =>{
+    const productId = parseInt(req.params.id);
+    const prodFinal = await manager.getProductById(productId);
+    res.send(prodFinal);
+});
+
+
+
+
+app.listen(8080, () => {
+    console.log("Server listening on port 8080");
+});
